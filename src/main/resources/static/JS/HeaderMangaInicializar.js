dfa2e8e72c5307e41e46fa9a4f4dce1207039580
Dismas <rosasfran.97@gmail.com>
2022-05-24 11:40:48 -0300

    JsDinamico

---
+++ src/main/resources/static/JS/HeaderMangaInicializar.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", getManga());
+
+async function getManga(){
+    const manga = await fetch('manga/1', {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        }
+    });
+    const mangajson = await manga.json();
+    resultado = '<div class="container h-100"><div class="row"><div class="col-12 col-md-3 text-center">'
+                +'<h1 class="book-type bg-manga mt-4">MANGA</h1><div class="element-image my-2">'
+                +'<img class="book-thumbnail " src="'+mangajson.port+'"></div></div>'
+                +'<div class="col-12 col-md-9 element-header-content-text"><h1 class="element-title my-2">'+mangajson.nombre
+                +'</h1><p class="element-description">'+mangajson.descripcion+'</p></div></div></div>';
+    document.getElementById("mangaHeader").innerHTML = resultado;
+}
