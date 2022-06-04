b38260c90e0586653fe0ea17df158d8fb140bb01
Dismas <rosasfran.97@gmail.com>
2022-06-04 20:02:13 -0300

    Leer manga listo

--- src/main/resources/static/JS/HeaderMangaInicializar.js
+++ src/main/resources/static/JS/HeaderMangaInicializar.js
@@ -4,6 +4,7 @@ async function getManga(){
     const valores = window.location.search;
     const urlParams = new URLSearchParams(valores);
     const ruta = 'manga/'+urlParams.get('id');
+
     const manga = await fetch(ruta, {
         method: 'GET',
         headers: {
@@ -17,23 +18,15 @@ async function getManga(){
                 +'<img class="book-thumbnail " src="'+mangajson.port+'"></div></div>'
                 +'<div class="col-12 col-md-9 element-header-content-text"><h1 class="element-title my-2">'+mangajson.nombre
                 +'</h1><p class="element-description">'+mangajson.descripcion+'</p></div></div></div>';
+
     document.getElementById("mangaHeader").innerHTML = resultado;
-
-     var capsAñadidos=[];
+    var capsAñadidos=[];
     for (var i=0; i<mangajson.caps.length; i++){
-    if(capsAñadidos.includes(mangajson.caps[i].num) == false)
-        {
-        capitulos.innerHTML+=' <div class="opcion"><p> Capitulo: '+mangajson.caps[i].num +'</p><p class="icono"> + </p></div>'
-        capsAñadidos.push(mangajson.caps[i].num);
+        if(capsAñadidos.includes(mangajson.caps[i].num) == false){
+            capitulos.innerHTML+='<a href="visualizado.html?capid='+mangajson.caps[i].id+'&mangaid='+mangajson.id+'"><div class="opcion"><p> Capitulo: '+mangajson.caps[i].num +'</p><p class="icono"> + </p></div></a>'
+            capsAñadidos.push(mangajson.caps[i].num);
+        }
     }
-
-    }
-
-   for (var i=0; i<mangajson.caps.length; i++){
-    verCap.innerHTML='<a href="#"> Enlace hacia capitulo </a>'
-
-    }
-
 }
 
 
